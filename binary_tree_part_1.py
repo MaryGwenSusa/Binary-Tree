5a458fcbcd48dcd66f47fddd88d584c80c293de4
Mary Gwen Susa <Mary Gwen G. Susa>
2023-01-14 11:01:41 +0800

    Tried adding duplicates to test the if condition in add_child method and it removed the duplicates upon printing

--- binary_tree_part_1.py
+++ binary_tree_part_1.py
@@ -49,6 +49,6 @@ def build_tree(elements):
 
 if __name__ == '__main__':
     #main method to execute a coroutine on the default event loop?
-    numbers = [17, 4, 1, 20, 9, 23, 18, 34]
+    numbers = [17, 4, 1, 20, 9, 23, 18, 34, 18, 4]
     numbers_tree = build_tree(numbers)
     print(numbers_tree.in_order_traversal())
