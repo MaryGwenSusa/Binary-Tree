6f83f206c777c97ef60d583fd8e639cff2a3bf9c
Mary Gwen Susa <Mary Gwen G. Susa>
2023-01-14 12:12:29 +0800

    Used the same structure of conditionals, storing, and recursive approach but in this order: left subtree and nodes -> right subtree and nodes -> root node-- to establish the sequence of post-order traversal

--- binary_tree_part_1.py
+++ binary_tree_part_1.py
@@ -52,6 +52,16 @@ class BinarySearchTreeNode:
     def post_order_traversal(self):
         elements = []
 
+        if self.left:
+            elements += self.left.post_order_traversal()
+
+        if self.right:
+            elements += self.right.post_order_traversal()
+
+        elements.append(self.data)
+
+        return elements
+
     
     def search(self, val):
         if self.data == val:
