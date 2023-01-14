06ddf8e64bcf480018b8f9d873f3c9c9b4f4c54a
Mary Gwen Susa <Mary Gwen G. Susa>
2023-01-14 11:33:21 +0800

    Just imported the class and method from another file so the code will be shortened

--- binary_tree.py
+++ binary_tree.py
@@ -1,72 +1,7 @@
-class BinarySearchTreeNode:
-    def __init__(self, data):
-        self.data = data
-        self.left = None
-        self.right = None
-
-    def add_child(self, data):
-        if data == self.data:
-            return
-
-        if data < self.data:
-            #add data in left subtree
-            if self.left:
-                self.left.add_child(data) #if self.left already has a value--recursively call the function to create another small subtree with another child value
-            else:
-                self.left = BinarySearchTreeNode(data)
-        else:
-            #add data in right subtree
-            if self.right:
-                self.right.add_child(data) #if self.right already has a value--recursively call the function to create another small subtree with another child value
-            else:
-                self.right = BinarySearchTreeNode(data)
-    
-    def in_order_traversal(self):
-        #returns the nodes in ascending order
-        elements = []
-
-        #visit left tree
-        if self.left:
-            elements += self.left.in_order_traversal()
-
-        #visit base/root node
-        elements.append(self.data)
-
-        #visit right tree
-        if self.right:
-            elements += self.right.in_order_traversal()
-
-        return elements
-    
-    def search(self, val):
-        if self.data == val:
-            return True
-        
-        if val < self.data:
-            #val might be in left subtree
-            if self.left:
-                """recursion"""
-                return self.left.search(val) 
-            else:
-                return False
-        
-        if val > self.data:
-            #val might be in right subtree
-            if self.right:
-                return self.right.search(val)
-            else:
-                return False
-
-        
-
-def build_tree(elements):
-    #helper method - takes elements as inputs
-    root = BinarySearchTreeNode(elements[0])
-
-    for i in range (1, len(elements)):
-        root.add_child(elements[i])
-    
-    return root
+from binary_tree_part_1 import(
+    BinarySearchTreeNode,
+    build_tree,  
+)
 
 if __name__ == '__main__':
     #main method to execute a coroutine on the default event loop?
